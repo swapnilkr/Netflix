abee215c57fb5876b15a9c5f978b83ea61f534d4
swapnilkr099 <swapnilkr099@gmail.com>
2021-10-29 18:07:46 +0530

    Featured component done using props type

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,13 +1,14 @@
 import Navbar from "../components/navbar/Navbar"
 import "./home.scss"
+import Featured from "../components/featured/Featured"
 
 const Home = () => {
     return (
         <div className="home">
+            {/* components */}
             <Navbar/>
-            <img 
-            width="100%"
-            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+            {/* props */}
+            <Featured type="movie"/>
         </div>
     )
 }
