08e87d3f3040bc9e27d05f451dcb706586c81ca9
swapnilkr099 <swapnilkr099@gmail.com>
2021-10-31 21:25:03 +0530

    List Item

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -1,10 +1,20 @@
 import "./listItem.scss"
+import {ThumbDownAltOutlined, ThumbUpAltOutlined, Add, PlayArrow } from "@material-ui/icons"
 
 export default function ListItem() {
     return (
         <div>
             <div className="listItem">
-                item
+                <img src="https://wallpapercave.com/wp/wp1558784.jpg" alt=""/>
+            <div className="itemInfo">
+                <div className="icons">
+                    <PlayArrow/>
+                    <Add/>
+                    <ThumbUpAltOutlined/>
+                    <ThumbDownAltOutlined/>
+                </div>
+            </div>
+            
             </div>
         </div>
     )
