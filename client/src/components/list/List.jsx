01cd72b6d145bb6cdefa118d7bd9d16a0c2cc151
swapnilkr099 <swapnilkr099@gmail.com>
2021-10-31 21:12:48 +0530

    Feature arrow set

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,8 +1,30 @@
 import "./list.scss"
 import {ArrowBackIosOutlined,ArrowForwardIos} from "@material-ui/icons"
+import ListItem from "../listItem/ListItem"
+import { useRef, useState } from "react";
 
+export default function List() {
 
-export default function list() {
+    // if we have 10 slides so after few clicks we will ran out of slides to handle this error we create function to repeat after finish
+    const [isMoved,setIsMoved] = useState(false);
+    const [slideNumber,setSlideNumber] = useState(0);
+
+    const listRef = useRef();
+    
+    // function to handle left and right arrow click
+    const handleClick =(direction) => {
+        setIsMoved(true);
+        let distance = listRef.current.getBoundingClientRect().x+-50;
+        if (direction=="left"  && slideNumber>0){
+            setSlideNumber(slideNumber-1);
+            listRef.current.style.transform = `translateX(${230+distance}px)`;
+        }
+        if (direction=="right"  && slideNumber <5 ){
+            setSlideNumber(slideNumber+1)
+            listRef.current.style.transform = `translateX(${-230+distance}px)`;
+        }
+    }
+
     return (
         <div className="list">
             <span className="listTitle">
@@ -10,13 +32,27 @@ export default function list() {
             </span>
 
             <div className="wrapper">
-                <ArrowBackIosOutlined/>
+                <ArrowBackIosOutlined 
+                className="slideArrow left" 
+                onClick={()=>handleClick("left")}
+                style={{display: !isMoved && "none"}}/>
 
-                <div className="container">
+                <div className="container" ref={listRef}>
+                    <ListItem/>
+                    <ListItem/>
+                    <ListItem/>
+                    <ListItem/>
+                    <ListItem/>
+                    <ListItem/>
+                    <ListItem/>
+                    <ListItem/>
+                    <ListItem/>
+                    <ListItem/>
                     
+
                 </div>
 
-                <ArrowForwardIos/>
+                <ArrowForwardIos className="slideArrow right" onClick={()=>handleClick("right")}/>
             </div>
 
         </div>
---
+++ client/src/components/listItem/ListItem.jsx
@@ -0,0 +1,11 @@
+import "./listItem.scss"
+
+export default function ListItem() {
+    return (
+        <div>
+            <div className="listItem">
+                item
+            </div>
+        </div>
+    )
+}
