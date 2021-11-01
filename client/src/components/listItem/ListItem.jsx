6d853810eb9ea65b1bf157adde9819490756ecb5
swapnilkr099 <swapnilkr099@gmail.com>
2021-11-01 21:22:30 +0530

    List Item styling

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -1,21 +1,46 @@
 import "./listItem.scss"
 import {ThumbDownAltOutlined, ThumbUpAltOutlined, Add, PlayArrow } from "@material-ui/icons"
+import { useRef, useState } from "react";
+
 
 export default function ListItem() {
+     const [isHovered,setIsHovered] = useState(false);
+
     return (
-        <div>
-            <div className="listItem">
+        
+            <div className="listItem" 
+            style={{left: isHovered && index *225-50}}
+            onMouseEnter={()=> setIsHovered(true)}
+            onMouseLeave={()=> setIsHovered(false)}>
                 <img src="https://wallpapercave.com/wp/wp1558784.jpg" alt=""/>
-            <div className="itemInfo">
-                <div className="icons">
-                    <PlayArrow/>
-                    <Add/>
-                    <ThumbUpAltOutlined/>
-                    <ThumbDownAltOutlined/>
+                <div className="itemInfo">
+                    <div className="icons">
+                        <PlayArrow/>
+                        <Add/>
+                        <ThumbUpAltOutlined/>
+                        <ThumbDownAltOutlined/>
+                    </div>
+                    <div className="itemInfoTop">
+                        <span>
+                            1 hour 14 mins
+                        </span>
+                        <span className="limit">
+                             +16
+                        </span>
+                        <span>
+                            1999
+                        </span>
+                    </div>
+                    <div className="desc">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                         Praesentium hic rem eveniet error possimus, neque ex doloribus.
+                    </div>
+                    <div className="genre">
+                        Action
+                    </div>
                 </div>
-            </div>
             
             </div>
-        </div>
+        
     )
 }
