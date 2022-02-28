2bc406f21c4a7795488af43a2fd03456afe084b9
swapnilkr099 <swapnilkr099@gmail.com>
2022-02-28 20:41:19 +0530

    Mongoose added

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+ 
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.listen(8800,()=>{
+    console.log("Backend server is running")
+})
